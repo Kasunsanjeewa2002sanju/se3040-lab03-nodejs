1ba5e4d883d29ff1e02dfcdf9eb33e6ec8e66d81
Kasun Sanjeewa <kasunsanjeewa2002@gmail.com>
2026-02-22 20:03:21 +0530

    Done promises

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const fs = require('fs');
-feature/kasun/read-file
-fs.writeFile('file.txt', 'Hello World!', function (err) {
- if (err) throw err;
- console.log('File saved!');
+const fs = require("fs");
+
+fs.writeFile("file.txt", "Hello World!", function (err) {
+  if (err) throw err;
+  console.log("File saved!");
 });
 
-fs.readFile('file.txt', 'utf8', function (err, data) {
- if (err) throw err;
- console.log(data);
+fs.readFile("file.txt", "utf8", function (err, data) {
+  if (err) throw err;
+  console.log(data);
 });
 
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve("Success!");
+  } else {
+    reject("Failure!");
+  }
+});
+myPromise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
