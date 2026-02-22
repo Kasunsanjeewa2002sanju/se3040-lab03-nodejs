e7ecef31f11cc37ed021aa816ee2ff4ee757c572
Kasun Sanjeewa <kasunsanjeewa2002@gmail.com>
2026-02-22 19:49:38 +0530

    Done create web server

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-feature/kasun/read-file
+
 fs.writeFile('file.txt', 'Hello World!', function (err) {
  if (err) throw err;
  console.log('File saved!');
@@ -10,3 +10,9 @@ fs.readFile('file.txt', 'utf8', function (err, data) {
  console.log(data);
 });
 
+const http = require('http');
+http.createServer(function (req, res) {
+ res.writeHead(200, {'Content-Type': 'text/html'});
+ res.write('Hello World!');
+ res.end();
+}).listen(8080);
